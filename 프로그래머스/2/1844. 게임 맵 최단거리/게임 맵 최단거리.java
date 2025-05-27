201dd9f15a9130c91bfeb8de82509edaa83b81e6
Park JiHun <wlgns679@gmail.com>
2025-05-27 17:12:29 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.70 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,46 +1,50 @@
 import java.util.*;
 
-
 class Solution {
     public int solution(int[][] maps) {
-        // bfs로 최단거리 구하기
-        
-        // 시작점 예약 (0,0, 거리 0)
-        int[] dr = {0, 1, 0, -1};
-        int[] dc = {1, 0, -1, 0};
-        
+        // 초기 값  7개
+        int[] dx  = {0, 1, 0, -1};
+        int[] dy  = {1, 0, -1, 0};
         int n = maps.length;
         int m = maps[0].length;
+        boolean[][] visited = new boolean[n][m];
         Queue<int[]> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[n][m];  
+        int depth = 1;
         
-        queue.add(new int[]{0, 0, 1});
+        // 첫 방문 표시
+        queue.add(new int[]{0,0});
         visited[0][0] = true;
         
+        // bfs 시작
         while(!queue.isEmpty()) {
-            // 현재 노드 방문
-            int[] cur = queue.poll();
-            int r = cur[0];
-            int c = cur[1];
-            int dist = cur[2];
+            int size = queue.size();
             
-            // if 도착지 도달하면 return dith;
-            if(r == n-1 && c == m-1) {
-                return dist;
-            }
-            
-            // 다음 노드 예약
-            for(int i = 0; i < 4; i++) {
-                int nr = r + dr[i]; 
-                int nc = c + dc[i];
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m && maps[nr][nc] == 1) {
-                    if(!visited[nr][nc]) {
-                        queue.add(new int[]{nr, nc, dist +1});
-                        visited[nr][nc] = true;
-                    }
+            // size 만큼 돌리기 (한 depth)
+            for (int i = 0; i < size; i++) {
+                int[] cur = queue.poll();
+                
+                // 도달했다면 depth 반환
+                if (cur[0] == n - 1 && cur[1] == m - 1) {
+                    return depth;
                 }
+                
+                // 상하좌우 이동 체크
+                for(int j = 0; j < 4; j ++) {
+                    int nx = cur[0] + dx[j];
+                    int ny = cur[1] + dy[j];
+                    
+                    if (nx >= 0 && ny >= 0 && nx < n && ny < m && maps[nx][ny] == 1 && !visited[nx][ny]) {
+                        queue.add(new int[]{nx, ny});
+                        visited[nx][ny] = true;
+                    }
+                    
+                }
+                
             }
+            depth += 1;
         }
+        
+        
         return -1;
     }
 }
