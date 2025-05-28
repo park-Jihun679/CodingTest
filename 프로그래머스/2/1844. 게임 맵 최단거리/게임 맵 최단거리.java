1ee6091396caa6487dbdedccec3ca5b1328253ba
Park JiHun <wlgns679@gmail.com>
2025-05-28 16:37:39 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.17 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,16 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
-        // 초기 값  7개
-        int[] dx  = {0, 1, 0, -1};
-        int[] dy  = {1, 0, -1, 0};
+        // 7개 정의
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
         int n = maps.length;
         int m = maps[0].length;
-        boolean[][] visited = new boolean[n][m];
-        Queue<int[]> queue = new LinkedList<>();
         int depth = 1;
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[n][m];
         
-        // 첫 방문 표시
+        // 첫 방문 
         queue.add(new int[]{0,0});
         visited[0][0] = true;
         
@@ -19,25 +19,21 @@ class Solution {
         while(!queue.isEmpty()) {
             int size = queue.size();
             
-            // size 만큼 돌리기 (한 depth)
-            for (int i = 0; i < size; i++) {
+            // size만큼 돌리기
+            for(int i = 0; i < size; i ++) {
                 int[] cur = queue.poll();
-                
-                // 도달했다면 depth 반환
                 if (cur[0] == n - 1 && cur[1] == m - 1) {
                     return depth;
                 }
                 
-                // 상하좌우 이동 체크
-                for(int j = 0; j < 4; j ++) {
+                for (int j = 0; j < 4; j++) {
                     int nx = cur[0] + dx[j];
                     int ny = cur[1] + dy[j];
                     
-                    if (nx >= 0 && ny >= 0 && nx < n && ny < m && maps[nx][ny] == 1 && !visited[nx][ny]) {
+                    if (nx >= 0 && ny >= 0 && nx < n && ny < m && !visited[nx][ny] && maps[nx][ny] == 1) {
                         queue.add(new int[]{nx, ny});
                         visited[nx][ny] = true;
                     }
-                    
                 }
                 
             }
@@ -45,6 +41,7 @@ class Solution {
         }
         
         
-        return -1;
+        int answer = -1;
+        return answer;
     }
 }
