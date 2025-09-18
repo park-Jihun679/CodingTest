fa664d87efc91ce6f819249526f8d59e32599b40
Park JiHun <wlgns679@gmail.com>
2025-09-18 17:02:18 +0900

    [level 2] Title: 타겟 넘버, Time: 7.07 ms, Memory: 92.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,18 @@
+class Solution {    
+    int answer = 0;
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0, 0);
+        return answer;
+    }
+    
+    void dfs(int[] numbers, int target, int depth, int sum) {
+        if (depth == numbers.length) {
+            if (sum == target) {
+                answer++;
+            }
+            return;
+        }
+        dfs (numbers, target, depth + 1, sum + numbers[depth]);
+        dfs (numbers, target, depth + 1, sum - numbers[depth]);
+    }
+}
