1c5fc32eb04f761dcdea4486e0adc79ac2ad7761
Park JiHun <wlgns679@gmail.com>
2025-10-01 01:34:04 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.32 ms, Memory: 56.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,38 +1,36 @@
 import java.util.*;
 
 class Solution {
+    int[] dx = {0, 1, 0, -1};
+    int[] dy = {1, 0, -1, 0};
     public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
-        int[] dx = {0, 1, 0, -1};
-        int[] dy = {1, 0, -1, 0};
-        int depth = 1;
-        Queue<int[]> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[n][m];
-        
-        queue.add(new int[]{0, 0});
+        int mx = maps.length;
+        int my = maps[0].length;
+        boolean[][] visited = new boolean[mx][my];
+        Queue<int[]> que = new LinkedList<>();
+        que.add(new int[]{0, 0});
         visited[0][0] = true;
         
-        while(!queue.isEmpty()) {
-            int size = queue.size();
+        int[][] dist = new int[mx][my];
+        dist[0][0] = 1;
+        
+        while(!que.isEmpty()) {
+            int[] cur = que.poll();
+            int x = cur[0];
+            int y = cur[1];
+            if (x == mx - 1 && y == my - 1) {
+                return dist[x][y];
+            }
             
-            for (int i = 0; i < size; i ++) {
-                int[] cur = queue.poll();
-                if (cur[0] == n - 1 && cur[1] == m - 1) {
-                    return depth;
-                }
-                
-                for (int j = 0; j < 4; j++) {
-                    int nx = cur[0] + dx[j];
-                    int ny = cur[1] + dy[j];
-                    if (nx >= 0 && ny >= 0 && nx < n && ny < m && !visited[nx][ny] && maps[nx][ny] == 1) {
-                        queue.add(new int[]{nx, ny});
-                        visited[nx][ny] = true;
-                    }
-                    
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx >= 0 && ny >= 0 && nx < mx && ny < my && maps[nx][ny] == 1 && !visited[nx][ny]) {
+                    que.add(new int[]{nx, ny});
+                    visited[nx][ny] = true;
+                    dist[nx][ny] = dist[x][y] + 1;
                 }
             }
-            depth += 1;
         }
         
         return -1;
