c592bf165c5f8828d66bd670fd87b5cead8410d5
Park JiHun <wlgns679@gmail.com>
2025-10-01 23:23:08 +0900

    [level 3] Title: 네트워크, Time: 0.24 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,23 +1,22 @@
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] isVisited = new boolean[n];
+        boolean[] visited = new boolean[n];
         
-        for(int i = 0; i < n; i++) {
-             if(!isVisited[i]) {
-                 dfs(i, computers, isVisited);
-                 answer ++;
-             }
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(computers, visited, i);
+                answer ++;
+            }
         }
         
         return answer;
     }
-    
-    void dfs(int target, int[][] computers, boolean[] isVisited) {
-        isVisited[target] = true;
-        for (int i = 0; i < isVisited.length; i++) {
-            if (!isVisited[i] && computers[target][i] == 1) {
-                dfs(i, computers, isVisited);
+    void dfs(int[][] computers, boolean[] visited, int cur) {
+        visited[cur] = true;
+        for (int i = 0; i < computers.length; i++) {
+            if (!visited[i] && computers[cur][i] == 1) {
+                dfs(computers, visited, i);
             }
         }
     }
