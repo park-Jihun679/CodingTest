ea94b25021505897e6b81f986f74f2daf2026745
Park JiHun <wlgns679@gmail.com>
2025-10-06 22:20:29 +0900

    [level 2] Title: 소수 찾기, Time: 5.48 ms, Memory: 9.57 MB -BaekjoonHub

--- Python3/프로그래머스/2/42839. 소수 찾기/README.md
+++ Python3/프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 9.37 MB, 시간: 7.72 ms
+메모리: 9.57 MB, 시간: 5.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 06일 21:45:31
+2025년 10월 06일 22:20:27
 
 ### 문제 설명
 
--- Python3/프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ Python3/프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,21 +1,22 @@
 from itertools import permutations
 
 def solution(numbers):
-    s_nums = set()
     answer = 0
+    set_nums = set()
     
     for i in range(1, len(numbers) + 1):
-        for n in permutations(numbers, i):
-            s_nums.add(int(''.join(n)))
+        for num in permutations(numbers, i):
+            set_nums.add(int(''.join(num)))
     
-    for n in s_nums:
-        if isPrime(n):
+    for s_num in set_nums:
+        if isPrime(s_num):
             answer += 1
-            
+
     return answer
 
 def isPrime(num):
     if num < 2: return False
     for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0: return False
+        if num % i == 0:
+            return False
     return True
