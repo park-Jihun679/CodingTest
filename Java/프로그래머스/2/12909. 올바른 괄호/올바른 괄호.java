57978da24e02b0fdcd583220b943d8cfacc2ac4a
Park JiHun <wlgns679@gmail.com>
2025-12-18 15:46:20 +0900

    [level 2] Title: 올바른 괄호, Time: 6.17 ms, Memory: 54.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,22 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int cnt = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if ('(' == s.charAt(i)) {
+                cnt ++;
+            } else {
+                cnt --;
+                if (cnt < 0) {
+                    return false;
+                }
+            }
+        }
+        if (cnt != 0) {
+            return false;
+        }
+
+    return answer;
+    }
+}
