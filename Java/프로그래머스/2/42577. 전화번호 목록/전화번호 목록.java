37ec0b223ff7f41856d47a2e57d986a6d526950e
Park JiHun <wlgns679@gmail.com>
2026-01-20 15:49:53 +0900

    [level 2] Title: 전화번호 목록, Time: 410.27 ms, Memory: 99.1 MB -BaekjoonHub

--- Java/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ Java/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,15 +2,18 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
+        boolean answer = true;
         Arrays.sort(phone_book);
         
         for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i+1].startsWith(phone_book[i])) {
-                return false;
+            if (phone_book[i+1].length() > phone_book[i].length() &&
+               phone_book[i+1].substring(0, phone_book[i].length()).equals(phone_book[i])) {
+                
+                answer = false;
+                break;
             }
         }
         
-        boolean answer = true;
         return answer;
     }
 }
